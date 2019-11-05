412a368647abcf5703b67a0a139f2c96e3f64e13
Amit Menashe <amttrt@gmail.com>
2019-11-05 19:07:33 +0200

    fixxing the hashtags

--- server.js
+++ server.js
@@ -19,121 +19,27 @@ let config = {
 };
 firebase.initializeApp(config);
 
-// //Fetch instances
-// app.get('/', function (req, res) {
-//   // var userReference = firebase.database().ref("/Users/");
-//   //
-//   // //Attach an asynchronous callback to read the data
-//   // userReference.on("value",
-//   //     function(snapshot) {
-//   //       console.log(snapshot.val());
-//   //       res.json(snapshot.val());
-//   //       userReference.off("value");
-//   //     },
-//   //     function (errorObject) {
-//   //       console.log("The read failed: " + errorObject.code);
-//   //       res.send("The read failed: " + errorObject.code);
-//   //     });
-// });
-
-//Create new instance
-// app.put('/', function (req, res) {
-//
-//   console.log("HTTP Put Request");
-//
-//   var userName = req.body.UserName;
-//   var name = req.body.Name;
-//   var age = req.body.Age;
-//
-//   var referencePath = '/Users/'+userName+'/';
-//   var userReference = firebase.database().ref(referencePath);
-//   userReference.set({Name: name, Age: age},
-//       function(error) {
-//         if (error) {
-//           res.send("Data could not be saved." + error);
-//         }
-//         else {
-//           res.send("Data saved successfully.");
-//         }
-//       });
-// });
-//
-// //Update existing instance
-// app.post('/', function (req, res) {
-//
-//   console.log("HTTP POST Request");
-//
-//   var userName = req.body.UserName;
-//   var name = req.body.Name;
-//   var age = req.body.Age;
-//
-//   var referencePath = '/Users/'+userName+'/';
-//   var userReference = firebase.database().ref(referencePath);
-//   userReference.update({Name: name, Age: age},
-//       function(error) {
-//         if (error) {
-//           res.send("Data could not be updated." + error);
-//         }
-//         else {
-//           res.send("Data updated successfully.");
-//         }
-//       });
-// });
-//
-// //Delete an instance
-// app.delete('/', function (req, res) {
-//
-//   console.log("HTTP DELETE Request");
-//   //todo
-// });
-
-function showHashtags(){
-  const query = firebase.database().ref('/hashtagsTop').orderByChild('count');
-  let stream = [];
-  let i = 0;
-  query.once('value', function (snapshot) {
-    snapshot.forEach(function (childSnapshot) {
-      var childKey = childSnapshot.key;
-      var childData = childSnapshot.val();
-      stream.push(childData);
-    })
-  }).then(() => reverse(stream));
-
-};
 
 function reverse(stream) {
   let revStream = [];
   while(stream.length>0) revStream.push(stream.pop());
   return revStream;
-    //console.log(stream.pop().count);
 }
 
-function funcV(word) {
-  let count = 0;
-  firebase.database().
-  ref('/hashtagsTop/'+word.substr(1)).once('value').
-  then(snp => {
-    if( snp.val().count >= 0)
-    {
-      //console.log("original " + snp.val().count);
-      count = snp.val().count+1;
-      //console.log("changed to " + count);
-    }
+function addHashtag(word) {
+  firebase.database().ref('/hashtagsTop/').child(word).transaction(function (count) {
+    return (count || 0) +1 } )
+}
+
+function crossItems() {
+  firebase.database().ref('/items/').on("value", function(snapshot) {
+    snapshot.forEach(v =>
+        v.val().caption.split(" ").forEach(word =>
+            word.charAt(0) === '#' ? addHashtag(word.slice(1)) : word.trim()
+        ));
   }
-  ).finally(() => firebase.database().ref('/hashtagsTop/'+word.substr(1)).update({
-      word: ""+word,
-      count: count,
-      lastUpdate: Date().toString()
-  }));
+  )
 }
-
-firebase.database().ref('/items').orderByChild('date').startAt(Date()).on(
-  "child_added", function (snapshot) {
-    snapshot.val().caption.split(" ").forEach(word => word.charAt(0) === '#' ?
-        funcV(word) : word.trim()
-      );
-    }
-);
 
 app.get('/orderPostsByDate', function (req, res) {
   console.log("OHYea");
@@ -147,11 +53,9 @@ app.get('/orderPostsByDate', function (req, res) {
     }.bind(this));
   });
    res.send(this.data);
-  //res.send({data: this.data, ids: this.ids});
 });
 
 app.get('/', function (req, res) {
-
   const query = firebase.database().ref('/hashtagsTop').orderByChild('count');
   let stream = [];
   let i = 0;
@@ -161,12 +65,9 @@ app.get('/', function (req, res) {
       var childData = childSnapshot.val();
       stream.push(childData);
     })
-    // }).then(() => reverse(stream));
   }).then(() => res.send(reverse(stream)));
-  //res.send('hello friendsssss');
 
   // after 3 days from the last lastUpdate we will  change the counter to 0
-
   firebase.database().ref('/hashtagsTop/').orderByChild('lastUpdate').on("value",  snapshot => {
     Object.values(snapshot.val()).map(counter => console.log(counter.lastUpdate))
   })
@@ -198,10 +99,6 @@ app.post('/addLikedBy/',function(req,res) {
   ref.child('likedBy').child(username).set(liked);
 });
 
-app.post('/removeLikedBy/',function(req,res) {
-
-});
-
 var server = app.listen(8080, function () {
 
   var host = server.address().address;
@@ -209,4 +106,5 @@ var server = app.listen(8080, function () {
 
   console.log("Example app listening at http://%s:%s", host, port);
 
+  crossItems();
 });
